6219c1769fd7fe387f393412bf38a3123474331d
northerntechie <northerntechie@github.com>
2024-03-01 21:24:05 -0800

    Removes sample menu

--- examples/api-samples/src/browser/api-samples-frontend-module.ts
+++ examples/api-samples/src/browser/api-samples-frontend-module.ts
@@ -19,7 +19,7 @@ import { bindDynamicLabelProvider } from './label/sample-dynamic-label-provider-
 import { bindSampleFilteredCommandContribution } from './contribution-filter/sample-filtered-command-contribution';
 import { bindSampleUnclosableView } from './view/sample-unclosable-view-contribution';
 import { bindSampleOutputChannelWithSeverity } from './output/sample-output-channel-with-severity';
-import { bindSampleMenu } from './menu/sample-menu-contribution';
+//import { bindSampleMenu } from './menu/sample-menu-contribution';
 import { bindSampleFileWatching } from './file-watching/sample-file-watching-contribution';
 import { bindVSXCommand } from './vsx/sample-vsx-command-contribution';
 import { bindSampleToolbarContribution } from './toolbar/sample-toolbar-contribution';
@@ -39,7 +39,7 @@ export default new ContainerModule((
     bindDynamicLabelProvider(bind);
     bindSampleUnclosableView(bind);
     bindSampleOutputChannelWithSeverity(bind);
-    bindSampleMenu(bind);
+    //bindSampleMenu(bind);
     bindSampleFileWatching(bind);
     bindVSXCommand(bind);
     bindSampleFilteredCommandContribution(bind);
